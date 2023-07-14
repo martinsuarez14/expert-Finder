bcf3b9d6bb3629fc55d37c7065a966e1db1cad3a
Agustin Pereyra <agus.pereyra835@gmail.com>
2023-07-14 19:06:29 -0300

    correcion de un bug en user controller

--- src/main/java/com/egg/expertfinder/controller/UserController.java
+++ src/main/java/com/egg/expertfinder/controller/UserController.java
@@ -1,10 +1,7 @@
 package com.egg.expertfinder.controller;
 
 import com.egg.expertfinder.entity.User;
-<<<<<<< HEAD
-=======
 import com.egg.expertfinder.service.UserService;
->>>>>>> e2cc4cbc04f5033ef4241ead7b3f31d410165294
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
